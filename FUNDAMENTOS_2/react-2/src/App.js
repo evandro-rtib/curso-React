b8cec1c57b3ecf0c98f3038a69ecb1accb2d8376
Evandro R Tiburcio <evandrorti@gmail.com>
2022-10-14 12:51:06 -0300

    Fundamentos_2: utilizando imagens, em assets

--- FUNDAMENTOS_2/react-2/src/App.js
+++ FUNDAMENTOS_2/react-2/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Img2 from "./assets/img2.jpg"
 function App() {
   return (
     <div className="App">
       {/*Imagens na pasta public*/}
       <img src="/img1.jpg" alt="Paisagem" />
+      <img src={Img2} alt="Outra Paisagem" />
+
     </div>
   );
 }
