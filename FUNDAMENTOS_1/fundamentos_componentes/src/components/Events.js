2183ded1bccb65ceea32603e1e457864c6f9d821
Evandro R Tiburcio <evandrorti@gmail.com>
2022-10-14 11:00:33 -0300

    Fundamentos_1: criando funções, funções desmembradas

--- FUNDAMENTOS_1/fundamentos_componentes/src/components/Events.js
+++ FUNDAMENTOS_1/fundamentos_componentes/src/components/Events.js
@@ -6,7 +6,19 @@ const Events = ()  =>{
 
     return(
         <div>
-            <button onClick={handleMyEvent}>Clique aqui</button>
+            <div>
+                <button onClick={handleMyEvent}>Clique aqui</button>
+            </div>
+            <div>
+                <button onClick={()=> console.log("clicou!")}>Clique aqui também</button>
+            </div>
+            <div>
+                <button onClick={()=> {
+                    if (true){
+                        console.log("Isso não é bom estar aqui");
+                    }
+                }}>Clique aqui também</button>
+            </div>
         </div>
     );    
 }
