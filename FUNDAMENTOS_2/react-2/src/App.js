3b46563e69b0f0b0d066c92eb07e0fbfa662634a
Evandro R Tiburcio <evandrorti@gmail.com>
2022-10-14 13:20:04 -0300

    Fundamentos_2: utilizando useState

--- FUNDAMENTOS_2/react-2/src/App.js
+++ FUNDAMENTOS_2/react-2/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Img2 from "./assets/img2.jpg"
+import ManageData from './components/ManageData';
 function App() {
   return (
     <div className="App">
       {/*Imagens na pasta public*/}
       <img src="/img1.jpg" alt="Paisagem" />
       <img src={Img2} alt="Outra Paisagem" />
+      <ManageData />
 
     </div>
   );
