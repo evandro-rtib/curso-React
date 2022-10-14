dd230d226baef611653c351c77a04029d0df8f2d
Evandro R Tiburcio <evandrorti@gmail.com>
2022-10-14 10:27:25 -0300

    Fundamentos_1: meu primeiro compnente

--- FUNDAMENTOS_1/fundamentos_componentes/src/App.js
+++ FUNDAMENTOS_1/fundamentos_componentes/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World React!</h1>
+      <h1>Fundamentos React Componentes</h1>
     </div>
   );
 }
