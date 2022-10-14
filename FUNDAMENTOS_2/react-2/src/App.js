83097b8b0410bb946f95058e5f9eff1cac14638d
Evandro R Tiburcio <evandrorti@gmail.com>
2022-10-14 12:12:43 -0300

    Fundamentos_2: criando funções, funções desmembradas

---
+++ FUNDAMENTOS_2/react-2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/*Imagens na pasta public*/}
+      <img src="/img1.jpg" alt="Paisagem" />
+    </div>
+  );
+}
+
+export default App;
